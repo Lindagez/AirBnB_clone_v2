37d17a9e691ab995e05d771c5b46e0f36ea97a04
@lindagez <lidiyagez@gmail.com>
2023-04-23 08:15:02 +0300

    web flask

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,55 +1,53 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application from flask import Flask, render_template"""
-from flask import Flask, render_template
+# importing flask
+'''
+script that starts a Flask web application
+'''
+from flask import Flask, escape, render_template
+
 app = Flask(__name__)
-app.jinja_env.trim_blocks = True
-app.jinja_env.lstrip_blocks = True
+
+# app decorator for routing
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-     """Displays 'Hello HBNB!'"""
+def hello():
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-     """Displays  HBNB!'"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
- """Displays 'C ', followed by the value of the text variable"""
-    return "C " + text.replace('_', ' ')
+def c_text(text):
+    new_txt = text.replace('_', ' ')
+    return 'C %s' % escape(new_txt)
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_is_cool(text='is_cool'):
-    """Displays 'Python '"""
-    return "Python " + text.replace('_', ' ')
+def py_lang(text='is cool'):
+
+    return 'Python %s' % text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def is_n_number(n):
- """Displays 'n is a number' if n is an integer"""
-    return "{:d} is a number".format(n)
+def num(n):
+    return '%d is a number' % n
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-        """Displays a HTML page with 'Number: n' """
-    return render_template('5-number.html', value=n)
+def render_no(n):
+    return render_template('5-number.html', no=n)
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def odd_or_even(n):
-        """Displays a HTML page with 'Number: n is even|odd' """
-    return render_template('6-number_odd_or_even.html', value=n)
+def even_odd(n):
+    return render_template('6-number_odd_or_even.html', no=n)
 
 
-if __name__ == "__main__":
-    """ Main Function """
+if __name__ == '__main__':
+    ''' run the app '''
     app.run(host='0.0.0.0', port=5000)
-
--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,49 +1,45 @@
 #!/usr/bin/python3
-""" Starts a Flask Web Application """
+'''
+starts a Flask web application
+'''
+
+# importing modules
+
+from flask import Flask, render_template
+
 from models import storage
 from models.state import State
-from os import environ
-from flask import Flask, render_template
+
 app = Flask(__name__)
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
 
 
 @app.teardown_appcontext
-def close_db(error):
-    """ Remove old SQLAlchemy Session """
+def teardown(self):
+    '''
+    remove the current SQLAlchemy Session
+    '''
     storage.close()
 
 
-@app.route('/states', strict_slashes=False)
-@app.route('/states/<id>', strict_slashes=False)
-def states_state(id=""):
-    """ displays a HTML page  """
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda k: k.name)
-    found = 0
-    state = ""
-    cities = []
-
-    for i in states:
-        if id == i.id:
-            state = i
-            found = 1
-            break
-    if found:
-        states = sorted(state.cities, key=lambda k: k.name)
-        state = state.name
-
-    if id and not found:
-        found = 2
-
-    return render_template('9-states.html',
-                           state=state,
-                           array=states,
-                           found=found)
+@app.route("/states", strict_slashes=False)
+@app.route("/states/<id>", strict_slashes=False)
+def stateById(id=None):
+    '''
+    Returns a state information
+    '''
+    states = list(storage.all(State).values())
+    if id is None:
+        return render_template(
+            "9-states.html",
+            display="multiple",
+            states=states,
+        )
+    else:
+        res = list(filter(lambda x: x.id == id, states))
+        state = res[0] if len(res) > 0 else False
+        return render_template("9-states.html", display="single", state=state)
 
 
-if __name__ == "__main__":
-    """ Main Function """
-    app.run(host='0.0.0.0', port=5000) 
-
+if __name__ == '__main__':
+    storage.reload()
+    app.run(host='0.0.0.0', port=5000)
