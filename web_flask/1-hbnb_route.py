81c47751d880d42b172a34e4079c5f1a43118edd
@lindagez <lidiyagez@gmail.com>
2023-04-23 07:49:17 +0300

    web flask

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-#script that starts a Flask web application  and serves two routes
-'''script that starts a Flask web application'''
+#importing flask
+'''
+script that starts a Flask web application
+'''
+
 from flask import Flask
+
 app = Flask(__name__)
+
 #app decorator
 
 @app.route('/', strict_slashes=False)
@@ -14,7 +19,7 @@ def hello():
 def hbnb():
     return 'HBNB'
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     '''main fun'''
    app.run(host='0.0.0.0', port=5000)
 
